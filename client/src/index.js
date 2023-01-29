5901e2800f006c9d5e68f2d23c62f33ab10b41cc
jesterb0206 <jesterb@seattleu.edu>
2023-01-28 20:26:03 -0800

    Imported Provider From React Redux and Store From the Global State Utils File

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import store from './utils/GlobalState';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // Passes the store object to all of the child components
+
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
