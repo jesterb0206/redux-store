c4b0f331d44f36ea4af57c83e975942edd2bc754
jesterb0206 <jesterb@seattleu.edu>
2023-01-30 23:21:01 -0800

    Refactored the ProductList Component to Use Redux

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,53 +1,59 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+import {useQuery} from '@apollo/client';
+import {QUERY_PRODUCTS} from '../../utils/queries';
+import {idbPromise} from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
+import {useSelector, useDispatch} from 'react-redux';
+import {updateProducts, selectProducts} from '../../slices/productSlice';
+import {selectCurrentCategory} from '../../slices/currentCategorySlice';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  // Get both the CurrentCategory and Products state
 
-  const { currentCategory } = state;
+  const currentCategory = useSelector(selectCurrentCategory);
+  const products = useSelector(selectProducts);
+  const dispatch = useDispatch();
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const {loading, data} = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
     if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
+      // Trigger the updateProducts reducer if the query has finished
+
+      dispatch(updateProducts(data.products));
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
     } else if (!loading) {
+      // Return the products from IndexedDB if the query is still happening
+
       idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
-        });
+        dispatch(updateProducts(products));
       });
     }
   }, [data, loading, dispatch]);
 
   function filterProducts() {
+    // If the user didn't select a category, return all of the products
+
     if (!currentCategory) {
-      return state.products;
+      return products;
     }
 
-    return state.products.filter(
+    // If the user selected a category, return only products from that category
+
+    return products.filter(
       (product) => product.category._id === currentCategory
     );
   }
 
   return (
-    <div className="my-2">
+    <div className='my-2'>
       <h2>Our Products:</h2>
-      {state.products.length ? (
-        <div className="flex-row">
+      {products.length ? (
+        <div className='flex-row'>
+          {/* Passes the product to the ProductItem component */}
           {filterProducts().map((product) => (
             <ProductItem
               key={product._id}
@@ -62,7 +68,7 @@ function ProductList() {
       ) : (
         <h3>You haven't added any products yet!</h3>
       )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+      {loading ? <img src={spinner} alt='loading' /> : null}
     </div>
   );
 }
