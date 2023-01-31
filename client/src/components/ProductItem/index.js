5c40db920a9ad99ea0a5ee64be518999bc58630e
jesterb0206 <jesterb@seattleu.edu>
2023-01-30 23:09:02 -0800

    Refactored the ProductItem Component to Use Redux

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,58 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
-import { idbPromise } from "../../utils/helpers";
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {pluralize} from '../../utils/helpers';
+import {idbPromise} from '../../utils/helpers';
+import {useSelector, useDispatch} from 'react-redux';
+import {
+  addToCart,
+  updateCartQuantity,
+  selectCart,
+} from '../../slices/cartSlice';
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  const cart = useSelector(selectCart);
+  const dispatch = useDispatch();
+  const {image, name, _id, price, quantity} = item;
 
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
-
-  const { cart } = state
-
-  const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+  const handleAddItem = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
     if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      });
+      // Increase the quantity by one if the item is already in the cart
+
+      dispatch(
+        updateCartQuantity({
+          ...itemInCart,
+          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+        })
+      );
       idbPromise('cart', 'put', {
         ...itemInCart,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
     } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
-      });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+      // Set the quantity to one if the item isn't already in the cart
+
+      dispatch(addToCart({...item, purchaseQuantity: 1}));
+      idbPromise('cart', 'put', {...item, purchaseQuantity: 1});
     }
-  }
+  };
 
   return (
-    <div className="card px-1 py-1">
+    <div className='card px-1 py-1'>
       <Link to={`/products/${_id}`}>
-        <img
-          alt={name}
-          src={`/images/${image}`}
-        />
+        <img alt={name} src={`/images/${image}`} />
         <p>{name}</p>
       </Link>
       <div>
-        <div>{quantity} {pluralize("item", quantity)} in stock</div>
+        <div>
+          {quantity} {pluralize('item', quantity)} in stock
+        </div>
         <span>${price}</span>
       </div>
-      <button onClick={addToCart}>Add to cart</button>
+      <button onClick={handleAddItem}>Add to cart</button>
     </div>
   );
 }
